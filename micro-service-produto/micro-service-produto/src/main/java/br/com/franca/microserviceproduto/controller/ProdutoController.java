63827a9da9bd66dcbfbb5e5b2377290eb9fd92ff
Tiago França <tiagofranca.rita@gmail.com>
2022-11-18 19:44:49 -0300

    Criação da API-PRODUTO parte II

---
+++ micro-service-produto/micro-service-produto/src/main/java/br/com/franca/microserviceproduto/controller/ProdutoController.java
@@ -0,0 +1,46 @@
+package br.com.franca.microserviceproduto.controller;
+
+import br.com.franca.microserviceproduto.entity.Produto;
+import br.com.franca.microserviceproduto.service.ProdutoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    public final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> buscarTodos(){
+
+        return ResponseEntity.ok(produtoService.listarTodos());
+
+    }
+
+    @PostMapping
+    public  ResponseEntity<Produto> salvar(@RequestBody Produto produto){
+
+         var novoProduto = produtoService.salvar(produto);
+
+
+        return new ResponseEntity(produto, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id){
+
+        produtoService.deletarProduto(id);
+
+        return  ResponseEntity.ok().build();
+    }
+
+
+}
