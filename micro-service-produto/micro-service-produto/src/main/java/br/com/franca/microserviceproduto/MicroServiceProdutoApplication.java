e9f44c0f8fb1fc27d0a16a9242bed44cf1496420
Tiago França <tiagofranca.rita@gmail.com>
2022-11-17 21:49:19 -0300

    Criacao do micro-service, alteracao do diagrama

---
+++ micro-service-produto/micro-service-produto/src/main/java/br/com/franca/microserviceproduto/MicroServiceProdutoApplication.java
@@ -0,0 +1,13 @@
+package br.com.franca.microserviceproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceProdutoApplication.class, args);
+	}
+
+}
---
+++ micro-service-produto/micro-service-produto/src/test/java/br/com/franca/microserviceproduto/MicroServiceProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.franca.microserviceproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
