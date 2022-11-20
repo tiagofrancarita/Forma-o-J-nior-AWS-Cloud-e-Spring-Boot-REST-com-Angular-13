126c09818c54b064acbd54ed99705e022896d0d7
Tiago França <tiagofranca.rita@gmail.com>
2022-11-20 13:43:19 -0300

    criacao buildspec

--- micro-service-produto/micro-service-produto/src/main/java/br/com/franca/microserviceproduto/MicroServiceProdutoApplication.java
+++ micro-service-produto/micro-service-produto/src/main/java/br/com/franca/microserviceproduto/MicroServiceProdutoApplication.java
@@ -9,6 +9,7 @@ public class MicroServiceProdutoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(MicroServiceProdutoApplication.class, args);
+		System.out.println("Inciada com sucesso");
 	}
 
 }
